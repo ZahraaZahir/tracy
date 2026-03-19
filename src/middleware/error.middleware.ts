f2c124f20c2d4255861e0c874e95b6738fb4eff5
Zahraa Zahir <zahraazahiirr@gmail.com>
2026-03-19 23:57:20 +0300

    feat: implement error handling middleware

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,22 @@
+import {Request, Response, NextFunction} from 'express';
+import {AppError} from '../errors/AppError.js';
+import {ZodError} from 'zod';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.error(`[ERROR] ${err.message}`);
+
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({error: err.message});
+  }
+
+  if (err instanceof ZodError) {
+    return res.status(400).json({error: 'Invalid data', details: err.format});
+  }
+
+  return res.status(500).json({error: 'Internal Server Error'});
+};
