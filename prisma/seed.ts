5d952b2f17046a117784a85005f6e110bd73b5a2
Zahraa Zahir <zahraazahiirr@gmail.com>
2026-03-20 02:11:43 +0300

    refactor: update seed file to use the same prisma singleton

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,4 @@
-import {PrismaClient} from '@prisma/client';
-import {PrismaPg} from '@prisma/adapter-pg';
-import pg from 'pg';
-import 'dotenv/config';
-
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new pg.Pool({connectionString});
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({adapter});
+import {prisma, pool} from '../src/lib/prisma.js';
 
 async function main() {
   console.log('Seeding NPCs...');
