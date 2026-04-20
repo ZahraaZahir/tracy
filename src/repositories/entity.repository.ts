9b17937ee4a8cb12747eb4d3aa9d1f36db565a2c
Zahraa Zahir <zahraazahiirr@gmail.com>
2026-04-20 13:23:26 +0300

    feat(entity): fetch all solution maps

--- src/repositories/entity.repository.ts
+++ src/repositories/entity.repository.ts
@@ -1,4 +1,8 @@
 import {prisma} from '../lib/prisma.js';
+import {
+  SolutionMapSchema,
+  SolutionValue,
+} from '../validators/inventory.validator.js';
 
 export class EntityRepository {
   async getEntityById(id: string) {
@@ -6,7 +10,19 @@ export class EntityRepository {
       where: {id},
     });
   }
+
   async countAllEntities() {
     return await prisma.gameEntity.count();
+  }
+
+  async getAllSolutionMaps(): Promise<
+    Array<{solutionMap: Record<string, SolutionValue>}>
+  > {
+    const entities = await prisma.gameEntity.findMany({
+      select: {solutionMap: true},
+    });
+    return entities.map((e) => ({
+      solutionMap: SolutionMapSchema.parse(e.solutionMap),
+    }));
   }
 }
