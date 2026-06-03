8c6c62bdbbe6cabda1e6984c85a573071fe65d08
Zahraa Zahir <zahraazahiirr@gmail.com>
2026-06-03 23:07:25 +0300

    feat: implement a global rate limit of a 100 requests per 15 min

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express, {Request, Response} from 'express';
+import rateLimit from 'express-rate-limit';
 import authRoutes from './routes/auth.routes.js';
 import worldRoutes from './routes/world.routes.js';
 import entityRoutes from './routes/entity.routes.js';
@@ -9,8 +10,19 @@ import {errorHandler} from './middleware/error.middleware.js';
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    message: 'Too many requests, try again in 15 minutes.',
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 app.use(express.json());
 app.use(cors());
+app.use(globalLimiter);
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/world', worldRoutes);
