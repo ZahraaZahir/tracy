e0ec98cecabf28cf12461fe0050f446d1c6a354a
sarabimar <sarabimar03@gmail.com>
2026-07-28 23:43:45 +0300

    Initialized and set up  Redis singleton and connection.

---
+++ src/lib/redis.ts
@@ -0,0 +1,17 @@
+import { Redis } from 'ioredis';
+
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
+
+const globalForRedis = global as unknown as { redis: Redis };
+
+export const redis =
+  globalForRedis.redis ||
+  new Redis(redisUrl, {
+    maxRetriesPerRequest: null, 
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForRedis.redis = redis;
+
+
+redis.on('connect', () => console.log('[REDIS] Connected to Leaderboard Store'));
+redis.on('error', (err) => console.error('[REDIS] Connection Error:', err));
