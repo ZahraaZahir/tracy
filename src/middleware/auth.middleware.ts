cc05697366b2d402f5c775cdeeb9c5878fbc3115
sarabimar <sarabimar03@gmail.com>
2026-07-31 20:44:01 +0300

    implemented custom username claim extraction and changed 'userId' to the conventional 'sub'

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -3,26 +3,18 @@ import jwt from 'jsonwebtoken';
 import {AuthenticatedRequest} from '../types/auth.types.js';
 import {UnauthorizedError} from '../errors/errors.js';
 
-export const authenticateToken = (
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction,
-) => {
+export const authenticateToken = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization'];
-
-  const secret = process.env.JWT_SECRET!;
-
-  if (!authHeader?.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Invalid or missing token.'));
-  }
+  if (!authHeader?.startsWith('Bearer ')) return next(new UnauthorizedError());
 
   const token = authHeader.split(' ')[1];
-
-  jwt.verify(token, secret, (err, user) => {
-    if (err) {
-      return next(new UnauthorizedError('Token expired or invalid.'));
-    }
-    req.user = user as {userId: string};
+  jwt.verify(token, process.env.JWT_SECRET!, (err, decoded: any) => {
+    if (err) return next(new UnauthorizedError('Token invalid.'));
+    
+    req.user = {
+      sub: decoded.sub,
+      username: decoded.username
+    };
     next();
   });
 };
--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -2,7 +2,7 @@ import {Request} from 'express';
 
 export interface AuthenticatedRequest extends Request {
   user?: {
-    userId: string;
+    sub: string;
     username: string;
   };
 }
